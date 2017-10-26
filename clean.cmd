c965368bf18f29df617563fe47d518a74ccea8a2
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2017-10-26 21:53:06 +0500

    Updating project files

--- clean.cmd
+++ clean.cmd
@@ -4,3 +4,9 @@ rmdir .\ArborGVT\obj /s /q
 
 rmdir .\ArborSample\bin /s /q
 rmdir .\ArborSample\obj /s /q
+
+rmdir .\java\JArborGVT\build /s /q
+rmdir .\java\JArborGVT\dist /s /q
+
+rmdir .\java\JArborSample\build /s /q
+rmdir .\java\JArborSample\dist /s /q
